b0730574dbe1d58758b1dcb101ab2c2585cca12b
GaneshSPatil <ganeshpl@thoughtworks.com>
2016-08-29 19:40:04 +0530

    Moved client out

--- client/client.js
+++
@@ -1,4 +0,0 @@
-var request = require('request');
-request('http://localhost:3000', function (error, response, body) {
-  console.log(response);
-});
--- client/test.js
+++
@@ -1,8 +0,0 @@
-var test = {};
-var assert = require('assert');
-
-test['should return true if the number is even'] = function(){
-  assert.equal(true, true);
-}
-
-exports.test = test;
