41cb3ea52ba8de7f15de62f229712148abf302a5
GaneshSPatil <ganeshpl@thoughtworks.com>
2016-08-30 09:00:54 +0530

    fii

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var id = require('./id.js').id;
 
 var app = express();
 
 app.get('/', function (req, res) {
   console.log('foo');
+  res.send('Admin Homepage');
+});
+
+app.post('/submit', function(req, res){
+  console.log(req);
   res.send('Admin Homepage');
 });
 
