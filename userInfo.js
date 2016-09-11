e659cf94b6849e678f61f41bd3796c080e973143
GaneshSPatil <ganeshpl@thoughtworks.com>
2016-09-11 16:47:49 +0530

    cleared userInfo registry

--- userInfo.js
+++ userInfo.js
@@ -1,3 +1,3 @@
 exports.info = {
-"foo":["should return true if the number is even","should return true if the number is odd","should return false if the number is even"]
+"foo":["should return true if the number is even"]
 }
